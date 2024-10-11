a7450f1e019e0728ff102a1c55226e6aa77694a9
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-10-11 14:02:01 +0300

    Complete exercise 10.1

---
+++ rate-repository-app/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>HELLO WORLD!</Text>
+      <Text>This React Native App is now initialized.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
